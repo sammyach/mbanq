d942b436fcffcff5ac85b81cad5d90c4522177ff
sammyach <sammyachman@gmail.com>
2022-05-31 23:23:27 +0100

    organization module: start

---
+++ src/app/manage-organization/manage-organization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-organization',
+  templateUrl: './manage-organization.component.html',
+  styleUrls: ['./manage-organization.component.scss']
+})
+export class ManageOrganizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
