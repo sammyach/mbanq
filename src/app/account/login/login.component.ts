1645fa0f78a8eb10acff720b45fa071efe4cf9c0
sammyach <sammyachman@gmail.com>
2022-06-03 00:21:07 +0100

    refactor ui

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,7 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
   submitted;
   loading = false;
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.createForm();
@@ -24,6 +26,26 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  onSubmit(){}
+  onSubmit(){
+    this.loading = true;
+    this.submitted = true;
+    if(this.form.valid){
+      const user = this.form.get('username').value;
+      const password = this.form.get('password').value;
+
+      if(user == 'a@a.com'){
+        this.auth.loggedIn = true;
+        this.router.navigate(['organization'])
+      }else{
+        this.loading = false;
+      }
+    }else{
+      this.loading = false;
+      // this.submitted = false;
+    }
+
+    console.log('from login: auth->', this.auth.loggedIn);
+
+  }
 
 }
